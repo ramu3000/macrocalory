09ff76602ce56888a5642e041b8f1746162a031d
Jussi Lampiselka <jlampise@iki.fi>
2018-03-29 11:21:59 +0300

    Change eslint-rule for switch-case

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -2,9 +2,9 @@ import { FETCH_USER } from '../actions/types';
 
 export default function(state = null, action) {
   switch (action.type) {
-  case FETCH_USER:
-    return action.payload || false;
-  default:
-    return state;
+    case FETCH_USER:
+      return action.payload || false;
+    default:
+      return state;
   }
 }
