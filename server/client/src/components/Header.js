395c7efd7de03bd7ce86d3872032eeec53e35c2b
Jussi Lampiselka <jlampise@iki.fi>
2018-04-07 16:43:53 +0300

    Use navbar in header.

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -2,61 +2,71 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import 'react-day-picker/lib/style.css';
-import { Button, Grid, Row, Col, Image, Panel } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import DatePicker from './DatePicker';
 
 class Header extends Component {
-  renderAuthButtons() {
-    if (this.props.auth.isLoading === true) {
-      return null;
-    } else if (this.props.auth.data === '') {
-      return (
-        <a href="/auth/google">
-          <Button className="btn_header" bsStyle="success">
-            Google Login
-          </Button>
-        </a>
-      );
-    } else {
-      return (
-        <a href="/api/logout">
-          <Button className="btn_header" bsStyle="danger">
-            Logout
-          </Button>
-        </a>
-      );
+  isLoggedIn() {
+    if (this.props.auth.data !== '') {
+      return true;
     }
+    return false;
   }
-  renderDatePicker() {
-    if (this.props.auth.isLoading) {
-      return null;
-    } else if (this.props.auth.data === '') {
-      return null;
-    } else {
-      return <DatePicker />;
-    }
+  isLoadingAuth() {
+    return this.props.auth.isLoading === true;
   }
 
+  renderAuthNav() {
+    if (this.isLoadingAuth()) {
+      return null;
+    } else if (!this.isLoggedIn()) {
+      return (
+        <Nav pullRight>
+          <NavItem href="/auth/google">Login with Google</NavItem>
+        </Nav>
+      );
+    }
+    return (
+      <Nav pullRight>
+        <NavItem href="/api/logout">Logout</NavItem>
+      </Nav>
+    );
+  }
+  renderDatePicker() {
+    if (this.isLoggedIn()) {
+      return <DatePicker />;
+    }
+    return null;
+  }
+
+  renderFeaturesNav() {
+    return (
+      <Nav>
+        {this.isLoggedIn() ? (
+          <NavItem>
+            <Link to="/dashboard">My Meals</Link>
+          </NavItem>
+        ) : null}
+      </Nav>
+    );
+  }
   render() {
     return (
-      <Panel>
-        <Grid>
-          <Row className="show-grid">
-            <Col lg={2} sm={2} md={2} xs={2}>
-              <Link to={this.props.auth.data !== '' ? '/dashboard' : '/'}>
-                <Image src="/images/KR_logo.png" alt="KR-Logo" thumbnail />
-              </Link>
-            </Col>
-            <Col lg={7} sm={7} md={7} xs={7}>
-              {this.renderDatePicker()}
-            </Col>
-            <Col />
-            <Col lg={3} sm={3} md={3} xs={3}>
-              {this.renderAuthButtons()}
-            </Col>
-          </Row>
-        </Grid>
-      </Panel>
+      <Navbar collapseOnSelect fluid>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to={this.isLoggedIn() ? '/dashboard' : '/'}>
+              KaloriRaptorit
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          {this.renderFeaturesNav()}
+          {this.renderDatePicker()}
+          {this.renderAuthNav()}
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
