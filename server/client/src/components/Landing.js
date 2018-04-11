c05fc7c9c1963397d1db260cd402e75c71fac41a
Jussi Lampiselka <jlampise@iki.fi>
2018-04-11 13:30:01 +0300

    Change redirecting when logging in.

--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-  return (
-    <h2>This is the Landing!</h2>
-  );
-};
+class Landing extends Component {
+  render() {
+    if (this.props.auth.loading === true) {
+      return (<h2>Logging in...</h2>);
+    }
+    else if (this.props.auth.data === '') {
+      return (<h2>Please log in to start healthy life with KaloriRaptorit.</h2>);
+    }
+    else {
+      return (<h2>Welcome!</h2>);
+    }
+    
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps, null)(Landing);
