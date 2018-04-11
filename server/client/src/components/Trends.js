9bc050acc083323e2cb1ba9fdb480defd9203711
Jussi Lampiselka <jlampise@iki.fi>
2018-04-11 14:37:14 +0300

    Add TrendsSettings-toggles

--- server/client/src/components/Trends.js
+++ server/client/src/components/Trends.js
@@ -1,16 +1,64 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import connect from 'react-redux/lib/connect/connect';
+import { Button, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
 
+class TrendsSettingsGroup extends React.Component {
+  constructor(props) {
+    super(props);
 
-class Trends extends Component {
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      startDate: new Date('2018-03-15T00:00:00'),
+      endDate: new Date(),
+      value: []
+    };
+  }
+
+  handleChange(e) {
+    this.setState({ value: e });
+  }
+
+  createTrends() {
+    if (this.state.value.length === 0) {
+      return null;
+    }
+  }
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <ToggleButtonGroup
+          type="checkbox"
+          value={this.state.value}
+          onChange={this.handleChange}
+        >
+          <ToggleButton value="protein">Protein</ToggleButton>
+          <ToggleButton value="carbohydrate">Carbohydrate</ToggleButton>
+          <ToggleButton value="fat">Fat</ToggleButton>
+          <ToggleButton value="energy">Energy</ToggleButton>
+          <ToggleButton value="water">Water</ToggleButton>
+        </ToggleButtonGroup>
+        <Button
+          bsStyle="primary"
+          onClick={this.createTrends.bind(this)}
+          disabled={this.state.value.length === 0}
+        >
+          GO!
+        </Button>
+      </div>
+    );
   }
 }
 
-//function mapStateToProps({}) {
-//  return {};
-//}
+class Trends extends Component {
+  render() {
+    return (
+      <div>
+        <TrendsSettingsGroup />
+      </div>
+    );
+  }
+}
 
 export default connect(null, null)(Trends);
