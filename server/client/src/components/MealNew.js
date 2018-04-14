32c63773eb265d973ab8cc016ef370e408c70cbf
Rasmus Laine <rasmus.laine@gmail.com>
2018-04-14 16:29:37 +0300

    export validate outside

--- server/client/src/components/MealNew.js
+++ server/client/src/components/MealNew.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 
 import { renderField, renderDateField, renderIngredients } from './parts/form/fields';
+import validate from './parts/form/validate';
 
 class MealNew extends Component {
 
@@ -75,35 +76,6 @@ class MealNew extends Component {
   }
 }
 
-function validate(values){
-  const errors = {}; 
-  //validate the inputs
-  if(!values.name){
-    errors.name = 'Enter a Meal name';
-  }
- 
-  if (!values.ingredients || !values.ingredients.length) {
-    errors.ingredients = { _error: 'At least one ingredient must be entered' };
-  } else {
-    const ingredientsArrayErrors = [];
-    values.ingredients.forEach((ingredient, ingredientIndex) => {
-      const ingredientErrors = {};
-      if (!ingredient || !ingredient.name) {
-        ingredientErrors.name = 'please fill the ingredient name';
-        ingredientsArrayErrors[ingredientIndex] = ingredientErrors;
-      }
-      if (!ingredient || !ingredient.mass) {
-        ingredientErrors.mass = 'please fill mass of ingredient';
-        ingredientsArrayErrors[ingredientIndex] = ingredientErrors;
-      }
-    });
-    if (ingredientsArrayErrors.length) {
-      errors.ingredients = ingredientsArrayErrors;
-    }
-  }
-  //return empty object means no errors and form is valid
-  return errors;
-}
 //getting props to this component
 function mapsStateToProps({ date }) {
   return {  date };
