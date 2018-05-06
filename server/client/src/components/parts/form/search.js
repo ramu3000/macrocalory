8c583c8601593561106d0410dac10f5a9c94b0b6
Rasmus Laine <rasmus.laine@gmail.com>
2018-05-06 21:36:45 +0300

    got dropdown and search function working correctly

--- server/client/src/components/parts/form/search.js
+++ server/client/src/components/parts/form/search.js
@@ -1,59 +1,71 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Field, FieldArray, reduxForm, arrayPush } from 'redux-form';
+import { Field, formValueSelector  } from 'redux-form';
 
 import { searchIngredients } from '../../../actions';
+
+
+const  renderSearchField = (field) => {
+  const { meta: { touched, error } } = field;
+  const colSize =  field.size ? field.size: '';
+  const className = `form-group ${colSize} ${ touched && error ? ' has-error' : ''} `;
+  
+  return (
+    <div className={className}>
+      <label>{field.label}</label>
+      <input
+        name={field.name}
+        placeholder={field.placeholder ? field.placeholder: ''}
+        className="form-control"
+        type="text"
+
+        {...field.input} //generate all input events, like equliant to examples of: onChange={field.input.onChange} or onFocus={field.input.onFocus} or onBlur....
+      />
+      
+      <div className="help-block">{touched ? error: ''}</div>
+    </div>
+  );
+};
+
+
 
 class SearchField extends Component {
 
   constructor (props) {
     super(props);
+    this.state = { foodsearch: '' };
   }
 
   searchIngredient(e) {
+    this.setState({foodsearch: e.target.value})
     if(e.target.value) {
       this.props.searchIngredients(e.target.value);
     }
   }
-  renderSearchField(field) {
-    const { meta: { touched, error } } = field;
-    const colSize =  field.size ? field.size: '';
-    const className = `form-group ${colSize} ${ touched && error ? ' has-error' : ''} `;
-   
-    
-    return (
-      <div className={className}>
-        <label>{field.label}</label>
-        <input
-          name={field.name}
-          placeholder={field.placeholder ? field.placeholder: ''}
-          className="form-control"
-          type="text"
-          {...field.input} //generate all input events, like equliant to examples of: onChange={field.input.onChange} or onFocus={field.input.onFocus} or onBlur....
-          onBlur={(e) => {
-            if((e.target.value).length >= 3){
-              this.searchIngredient(e);
-            }
-          }}
-        />
-        
-        <div className="help-block">{touched ? error: ''}</div>
-      </div>
-    );
-  }
+
   addIngredient() { 
 
+    let ingredientsSuggestions = this.props.ingredients ? (this.props.ingredients.data ? this.props.ingredients.data : []) : [];
+
     let specificValue = (ins) => {
+      
       this.props.chosen(ins);
+      this.setState({foodsearch: '' })
+
     };
-    console.log(this.props);
-    if(this.props.ingredients.length === 0 ) {
-      return <div></div>;
-    }
+
     return (
-      this.props.ingredients.data.map(function(ingredient, index) {
+      ingredientsSuggestions.map(function(ingredient, index) {
         return (
-          <li className="list-group-item" onClick={() => specificValue(ingredient) } key={index}>{ingredient.name}</li>
+          <li className="list-group-item" 
+            onClick={() => {
+              specificValue(ingredient);
+            } 
+            } 
+            key={index}
+          >
+            {ingredient.name}
+          </li>
         );
         
       })
@@ -61,18 +73,20 @@ class SearchField extends Component {
   }
 
   render() {
-
+    console.log(this.state);
     return (
       <div>
         <Field 
-          name="test"
+          name="searchIngredient"
           label="Search Ingredient"
-          component={ this.renderSearchField.bind(this) }
+          component={ renderSearchField }
+          onChange={this.searchIngredient.bind(this)}
+          value={this.state.foodsearch && ''}
         />
         <div>
           <ul className="ingredients list-group list-group-hover list-product-search scrollbar">
             {
-              this.addIngredient()
+              this.state.foodsearch.length > 3 && this.addIngredient()
             }
           </ul>
         </div>
@@ -83,9 +97,10 @@ class SearchField extends Component {
 
 
 //getting props to this component
-function mapsStateToProps({ ingredients }) {
-  return {   ingredients };
+function mapsStateToProps({ ingredients, ownProps }) {
+  return {   ingredients,ownProps };
 }
+
 
 //and add redux connection
 export default connect(mapsStateToProps, {searchIngredients })(SearchField);
